74e46d5a6cd680bdd9cd57cf19ab628ca39958d1
GitHub Action <action@github.com>
2024-06-25 06:58:42 +0000

    Initialize template repository proper
    Set package name to com.github.reeceauld.scatha
    Set project name to scatha
    Set modid to scatha

--- src/main/java/com/github/reeceauld/scatha/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/reeceauld/scatha/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.reeceauld.scatha.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
